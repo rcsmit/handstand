b6710a64702eb47428366021e22211562ea52748
Rene Smit <rcx.smit@gmail.com>
2025-12-13 13:45:51 +0700

    ..

--- app.py
+++ app.py
@@ -456,13 +456,9 @@ def run(run_streamlit, stframe, filetype, input_file, output_file, detection_con
                 with col2:
                     st.metric("Symmetry Score", f"{symmetry_score:.1f}/100")
                 with col3:
-<<<<<<< HEAD
                     # combined = (total_score * 0.7 + symmetry_score * 0.3)
                     combined = (total_score * COMBINED_FACTOR + symmetry_score * (1-COMBINED_FACTOR))
                     
-=======
-                    combined = (total_score * COMBINED_FACTOR + symmetry_score * (1-COMBINED_FACTOR))
->>>>>>> 3dc0149327b850c90a577767dfd2a87e36cebbf3
                     st.metric("Overall Score", f"{combined:.1f}/100")
                 
                 # Grade
