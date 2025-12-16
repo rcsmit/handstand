ba32910a876d39a642358490e1e1ee78a1fa9b63
Rene Smit <rcx.smit@gmail.com>
2025-12-16 11:51:31 +0700

    ...25i

--- app.py
+++ app.py
@@ -384,10 +384,10 @@ def process_frame(image, pose, mp_pose, mp_drawing, drawing_spec, drawing_spec_p
 
         # Convert back and draw landmarks
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        mp_drawing.draw_landmarks(
-            image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-            drawing_spec_points, connection_drawing_spec=drawing_spec
-        )
+        # mp_drawing.draw_landmarks(
+        #     image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+        #     drawing_spec_points, connection_drawing_spec=drawing_spec
+        # )
 
         # Resize for display
         final_frame = cv2.resize(image, (0, 0), fx=0.5, fy=0.5)
